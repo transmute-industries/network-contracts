d86df5c72dd74079e31a3aca2924bb1a7a8a4582
Guillaume <guillaumegorp@gmail.com>
2018-06-18 17:06:11 +0200

    Add bond function and Delegator struct

--- contracts/ProviderRound.sol
+++ contracts/ProviderRound.sol
@@ -4,22 +4,38 @@ import "./TransmuteToken.sol";
 
 contract ProviderRound is TransmuteToken {
 
-  struct ProviderParameters {
+  struct Delegator {
+    address delegatorAddress;
+    address delegateAddress;
+    uint amountBonded;
+  }
+
+  uint public numberOfDelegators;
+  mapping(uint => Delegator) public delegators;
+
+  struct Provider {
     address providerAddress;
     uint pricePerStorageMineral;
     uint pricePerComputeMineral;
     uint blockRewardCut;
     uint feeShare;
+    uint totalAmountBonded;
   }
 
   uint public numberOfProviderCandidates;
-
-  mapping (uint => ProviderParameters) public providerCandidates;
+  mapping(uint => Provider) public providerCandidates;
 
   function provider(uint _pricePerStorageMineral, uint _pricePerComputeMineral, uint _blockRewardCut, uint _feeShare) public {
     require(0 <= _blockRewardCut && _blockRewardCut <= 100);
     require(0 <= _feeShare && _feeShare <= 100);
-    uint candidateId = numberOfProviderCandidates++;
-    providerCandidates[candidateId] = ProviderParameters(msg.sender, _pricePerStorageMineral, _pricePerComputeMineral, _blockRewardCut, _feeShare);
+    uint providerCandidateId = numberOfProviderCandidates++;
+    providerCandidates[providerCandidateId] = Provider(msg.sender, _pricePerStorageMineral, _pricePerComputeMineral, _blockRewardCut, _feeShare, 0);
+  }
+
+  function bond(uint _providerCandidateId, uint _amount) public {
+    Provider storage providerCandidate = providerCandidates[_providerCandidateId];
+    uint delegatorId = numberOfDelegators++;
+    delegators[delegatorId] = Delegator(msg.sender, providerCandidate.providerAddress, _amount);
+    providerCandidate.totalAmountBonded += _amount;
   }
 }
