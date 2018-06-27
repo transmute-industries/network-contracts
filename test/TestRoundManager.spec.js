b40d0eb8f0d2bd6280b618637f72d76f4d6246b1
Guillaume <guillaumegorp@gmail.com>
2018-06-28 00:43:31 +0200

    Remove beforeEach

--- test/TestRoundManager.spec.js
+++ test/TestRoundManager.spec.js
@@ -10,18 +10,18 @@ contract('RoundManager', accounts => {
     before(async () => {
       rm = await RoundManager.deployed();
       electionPeriodLength = await rm.electionPeriodLength.call();
-    });
-
-    beforeEach(async () => {
-      // Adds block so that the next block is the beginning of a next round 
-      // ie adds blocks so that (blockNumber + 1) % electionPeriodLength === 0
       await blockMiner.mineUntilEndOfElectionPeriod(rm);
       assert.equal(0, (web3.eth.blockNumber + 1) % electionPeriodLength);
-      await rm.initializeRound();
     });
 
     it('should initialize the round', async () => {
+      await rm.initializeRound();
       assert.equal(1, await rm.roundNumber.call());
+    });
+
+    it('should initialize the next round electionPeriodLength blocks after initializing the last round', async () => {
+      await blockMiner.mine(electionPeriodLength - 1);
+      await rm.initializeRound();
     });
 
     it('should fail to initialize a new round one block after initializing the last round', async () => {
@@ -29,16 +29,9 @@ contract('RoundManager', accounts => {
     });
 
     it('should fail to initialize a new round electionPeriodLength - 1 block after initializing the last round', async () => {
-      // Adds electionPeriodLength - 2 blocks
-      await blockMiner.mine(electionPeriodLength - 2);
-      // Adds one block.
-      // It should fail because we are still in the current election period (electionPeriodLength - 2 + 1 = electionPeriodLength - 1)
+      await blockMiner.mine(electionPeriodLength - 3);
+      // Here the next initializeRound() call will happen on the (electionPeriodLength - 1)th block of the current round
       await assertFail(rm.initializeRound());
-    });
-
-    it('should initialize the next round electionPeriodLength blocks after initializing the last round', async () => {
-      await blockMiner.mine(electionPeriodLength - 1);
-      await rm.initializeRound();
     });
   });
 });
