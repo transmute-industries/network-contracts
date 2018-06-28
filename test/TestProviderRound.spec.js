238613d9a38cd649df1c751c1aef031a38ccb06c
Guillaume <guillaumegorp@gmail.com>
2018-06-28 14:19:32 +0200

    Fix tests

--- test/TestProviderRound.spec.js
+++ test/TestProviderRound.spec.js
@@ -4,6 +4,7 @@ const { blockMiner, assertFail } = require('./utils.js');
 contract('ProviderRound', accounts => {
 
   let providerRound;
+  const PROVIDER_REGISTERED = 1;
 
   describe('provider', () => {
 
@@ -19,15 +20,15 @@ contract('ProviderRound', accounts => {
     it("should register a provider's parameters", async () => {
       await providerRound.provider(22, 10, 1, 25, {from: accounts[0]});
       await providerRound.provider(10, 20, 2, 35, {from: accounts[1]});
-      let firstProvider = await providerRound.providerCandidates.call(0);
-      let secondProvider = await providerRound.providerCandidates.call(1);
-      assert(firstProvider[0] == accounts[0]
+      let firstProvider = await providerRound.providers.call(accounts[0]);
+      let secondProvider = await providerRound.providers.call(accounts[1]);
+      assert(firstProvider[0] == PROVIDER_REGISTERED
         && firstProvider[1].toNumber() == 22
         && firstProvider[2].toNumber() == 10
         && firstProvider[3].toNumber() == 1
         && firstProvider[4].toNumber() == 25
       );
-      assert(secondProvider[0] == accounts[1]
+      assert(secondProvider[0] == PROVIDER_REGISTERED
         && secondProvider[1].toNumber() == 10
         && secondProvider[2].toNumber() == 20
         && secondProvider[3].toNumber() == 2
@@ -47,8 +48,7 @@ contract('ProviderRound', accounts => {
     });
 
     it('should set totalBondedAmount to 0', async () => {
-      const firstProvider = await providerRound.providerCandidates.call(0);
-      assert.equal(accounts[0], firstProvider[0]);
+      const firstProvider = await providerRound.providers.call(accounts[0]);
       assert.equal(0, firstProvider[5]);
     });
 
@@ -87,54 +87,54 @@ contract('ProviderRound', accounts => {
     });
 
     it('should fail if the delegator does not approve to transfer at least the bonded amount', async () => {
-      await assertFail( providerRound.bond(0, 10, {from: accounts[5]}) );
+      await assertFail( providerRound.bond(accounts[0], 10, {from: accounts[5]}) );
       await providerRound.approve(contractAddress, 9, {from: accounts[5]});
-      await assertFail( providerRound.bond(0, 10, {from: accounts[5]}) );
+      await assertFail( providerRound.bond(accounts[0], 10, {from: accounts[5]}) );
     });
 
     it('should add a delegator to the delegators list', async () => {
       await providerRound.approve(contractAddress, 10, {from: accounts[5]});
-      await providerRound.bond(0, 10, {from: accounts[5]});
+      await providerRound.bond(accounts[0], 10, {from: accounts[5]});
       const firstDelegator = await providerRound.delegators.call(accounts[5]);
       assert.equal(accounts[0], firstDelegator[0]);
       assert.equal(10, firstDelegator[1]);
     });
 
     it('should increase the totalAmountBonded of the provider', async () => {
-      let firstProvider = await providerRound.providerCandidates.call(0);
+      let firstProvider = await providerRound.providers.call(accounts[0]);
       assert.equal(10, firstProvider[5]);
 
       await providerRound.approve(contractAddress, 20, {from: accounts[6]});
-      await providerRound.bond(1, 20, {from: accounts[6]});
+      await providerRound.bond(accounts[1], 20, {from: accounts[6]});
       await providerRound.approve(contractAddress, 50, {from: accounts[7]});
-      await providerRound.bond(1, 50, {from: accounts[7]});
-      firstProvider = await providerRound.providerCandidates.call(0);
-      const secondProvider = await providerRound.providerCandidates.call(1);
+      await providerRound.bond(accounts[1], 50, {from: accounts[7]});
+      firstProvider = await providerRound.providers.call(accounts[0]);
+      const secondProvider = await providerRound.providers.call(accounts[1]);
       assert.equal(10, firstProvider[5]);
       assert.equal(70, secondProvider[5]);
     });
 
     it('should fail if no providerCandidate is associated with the given providerCandidateId', async () => {
-      await assertFail( providerRound.bond(2, 15, {from: accounts[5]}) );
+      await assertFail( providerRound.bond(accounts[2], 15, {from: accounts[5]}) );
     });
 
     it('should fail if called twice by the same delegator', async () => {
       await providerRound.approve(contractAddress, 40, {from: accounts[8]});
-      await providerRound.bond(1, 40, {from: accounts[8]});
-      await assertFail( providerRound.bond(1, 40, {from: accounts[8]}) );
+      await providerRound.bond(accounts[1], 40, {from: accounts[8]});
+      await assertFail( providerRound.bond(accounts[1], 40, {from: accounts[8]}) );
     });
 
     it('should fail if TST balance is less than bonded amount', async () => {
       await providerRound.approve(contractAddress, 1001, {from: accounts[9]});
-      await assertFail( providerRound.bond(1, 1001, {from: accounts[9]}) )
-      assert.equal(110, (await providerRound.providerCandidates(1))[5]);
+      await assertFail( providerRound.bond(accounts[1], 1001, {from: accounts[9]}) )
+      assert.equal(110, (await providerRound.providers(accounts[1]))[5]);
     });
 
     it("should transfer amount from the delegator's balance to the contract's balance", async () => {
       const contractBalance = (await providerRound.balanceOf(providerRound.address)).toNumber();
       assert.equal(1000, await providerRound.balanceOf(accounts[9]));
       await providerRound.approve(contractAddress, 300, {from: accounts[9]});
-      await providerRound.bond(1, 300, {from: accounts[9]});
+      await providerRound.bond(accounts[1], 300, {from: accounts[9]});
       assert.equal(700, await providerRound.balanceOf(accounts[9]));
       assert.equal(contractBalance + 300, await providerRound.balanceOf(providerRound.address));
     });
