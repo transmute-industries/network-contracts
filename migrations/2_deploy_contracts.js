01ac6c325dd50e5ea12089aacdcc8f344fd2ae65
Guillaume <guillaumegorp@gmail.com>
2018-07-02 21:42:21 +0200

    Add ProviderPool contract that uses Livepeer's DoubleLinkedList library

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,14 @@
 const TST = artifacts.require('./TransmuteToken.sol');
 const ProviderRound = artifacts.require('./ProviderRound.sol');
 const RoundManager = artifacts.require('./RoundManager.sol');
+const SortedDoublyLL = artifacts.require('./SortedDoublyLL.sol');
+const TestProviderPool = artifacts.require('./TestProviderPool.sol');
 
 module.exports = deployer => {
   deployer.deploy(TST);
   deployer.deploy(ProviderRound);
   deployer.deploy(RoundManager);
+  deployer.deploy(SortedDoublyLL);
+  deployer.link(SortedDoublyLL, TestProviderPool);
+  deployer.deploy(TestProviderPool);
 };
