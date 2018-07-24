be9b644c432d1667fcec1a9952c547c9eb558e46
Guillaume <guillaumegorp@gmail.com>
2018-07-24 13:46:43 +0200

    Add JobManager with submitMineral method

---
+++ contracts/JobManager.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.24;
+
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract JobManager {
+  using SafeMath for uint;
+
+  event MineralAdded (
+    uint id,
+    string name
+  );
+
+  struct Mineral {
+    string name;
+  }
+
+  uint numberOfMinerals;
+  mapping(uint => Mineral) minerals;
+
+  function submitMineral(string _name) external {
+    minerals[numberOfMinerals] = Mineral(_name);
+    emit MineralAdded(numberOfMinerals, _name);
+    numberOfMinerals = numberOfMinerals.add(1);
+  }
+}
