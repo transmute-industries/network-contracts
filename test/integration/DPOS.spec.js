ce8fb9166da9a5c5ebd0ab16dab0c320e2c25a2e
Guillaume <guillaumegorp@gmail.com>
2018-07-29 18:50:03 +0200

    Add tests for unbonding and withdrawing

--- test/integration/DPOS.spec.js
+++ test/integration/DPOS.spec.js
@@ -10,8 +10,13 @@ contract('integration/TransmuteDPOS', (accounts) => {
   const PROVIDER_UNREGISTERED = 0;
   const PROVIDER_REGISTERED = 1;
 
+  // Delegator states
+  const DELEGATOR_UNBONDED = 0;
+  const DELEGATOR_UNBONDED_WITH_TOKENS_TO_WITHDRAW = 1;
+  const DELEGATOR_BONDED = 2;
+
   let provider1; let provider2; let provider3; let provider4; let provider5;
-  let delegator1; let delegator2; let delegator3; let delegator4;
+  let delegator1; let delegator2; let delegator3; let delegator4; let delegator5;
 
   async function reset() {
     tdpos = await TransmuteDPOS.new();
@@ -137,7 +142,7 @@ contract('integration/TransmuteDPOS', (accounts) => {
   });
 
 
-  describe('Delegating tokens', () => {
+  describe('Bonding tokens', () => {
     before(async () => {
       await reset();
       provider1 = accounts[1];
@@ -190,6 +195,60 @@ contract('integration/TransmuteDPOS', (accounts) => {
     it('delegator4 delegates to himself', async () => {
       await tdpos.approve(contractAddress, 100, {from: delegator4});
       await tdpos.bond(delegator4, 100, {from: delegator4});
+    });
+  });
+
+  describe('Unbonding tokens', () => {
+    before(async () => {
+      delegator5 = accounts[7];
+    });
+
+    it('delegator1 unbonds and enters the UNBONDED_WITH_TOKENS_TO_WITHDRAW state', async () => {
+      assert.equal(DELEGATOR_BONDED, await tdpos.delegatorStatus(delegator1));
+      await tdpos.unbond({from: delegator1});
+      assert.equal(DELEGATOR_UNBONDED_WITH_TOKENS_TO_WITHDRAW, await tdpos.delegatorStatus(delegator1));
+    });
+
+    it('delegator1 unbonds again but fails because he is in the UNBONDED_WITH_TOKENS_TO_WITHDRAW state', async () => {
+      assert.equal(DELEGATOR_UNBONDED_WITH_TOKENS_TO_WITHDRAW, await tdpos.delegatorStatus(delegator1));
+      await assertFail( tdpos.unbond({from: delegator1}) );
+    });
+
+    it('provider1 unbonds and as a result looses his Provider status', async () => {
+      assert.equal(PROVIDER_REGISTERED, await tdpos.providerStatus(provider1));
+      await tdpos.unbond({from: provider1});
+      assert.equal(PROVIDER_UNREGISTERED, await tdpos.providerStatus(provider1));
+    });
+
+    it('delegator5 unbonds but fails because he is in the UNBONDED state', async () => {
+      assert.equal(DELEGATOR_UNBONDED, await tdpos.delegatorStatus(delegator5));
+      await assertFail( tdpos.unbond({from: delegator5}) );
+    });
+  });
+
+  describe('Withdrawing tokens', () => {
+    it('delegator1 withdraws and fails because he didn\'t wait the unbondingPeriod', async () => {
+      assert.equal(DELEGATOR_UNBONDED_WITH_TOKENS_TO_WITHDRAW, await tdpos.delegatorStatus(delegator1));
+      await assertFail( tdpos.withdraw({from: delegator1}) );
+    });
+
+    it('delegator1 waits the unbondingPeriod and withdraws his tokens', async () => {
+      const withdrawInformation = await tdpos.withdrawInformations.call(delegator1);
+      const withdrawBlock = withdrawInformation[0];
+      await blockMiner.mineUntilBlock(withdrawBlock);
+      await tdpos.withdraw({from: delegator1});
+      assert.equal(DELEGATOR_UNBONDED, await tdpos.delegatorStatus(delegator1));
+    });
+
+    it('provider1 withdraws his tokens and enters UNBONDED state', async () => {
+      assert.equal(DELEGATOR_UNBONDED_WITH_TOKENS_TO_WITHDRAW, await tdpos.delegatorStatus(provider1));
+      await tdpos.withdraw({from: provider1});
+      assert.equal(DELEGATOR_UNBONDED, await tdpos.delegatorStatus(provider1));
+    });
+
+    it('delegator4 withdraws but fails because he is in the BONDED state', async () => {
+      assert.equal(DELEGATOR_BONDED, await tdpos.delegatorStatus(delegator4));
+      await assertFail( tdpos.withdraw({from: delegator4}) );
     });
   });
 });
