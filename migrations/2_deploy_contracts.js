42b06b7a754ddaf7f03605923d7b93fb4c038720
Guillaume <guillaumegorp@gmail.com>
2018-07-31 12:59:48 +0200

    Fix deploy file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,10 +8,11 @@ const JobManager = artifacts.require('./JobManager.sol');
 module.exports = (deployer) => {
   deployer.deploy(TST);
   deployer.deploy(SortedDoublyLL);
-  deployer.link(SortedDoublyLL, RoundManager);
-  deployer.deploy(RoundManager);
   deployer.link(SortedDoublyLL, TestProviderPool);
   deployer.deploy(TestProviderPool);
+  deployer.link(SortedDoublyLL, RoundManager);
+  deployer.deploy(RoundManager);
+  deployer.link(SortedDoublyLL, TestTransmuteDPOS);
   deployer.deploy(TestTransmuteDPOS);
   deployer.deploy(JobManager);
 };
