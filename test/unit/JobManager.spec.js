e9d3dc90da52d812749f9d18ae76f162e1369354
gjgd <gjgd@protonmail.com>
2018-08-17 15:44:36 -0500

    Add tests for getPseudoRandomNumber

--- test/unit/JobManager.spec.js
+++ test/unit/JobManager.spec.js
@@ -111,4 +111,63 @@ contract('JobManager', (accounts) => {
       assert.deepEqual(numberOfJobs.add(1), await jm.numberOfJobs.call());
     });
   });
+
+  describe.only('getPseudoRandomNumber', () => {
+    before(async () => {
+      jm = await JobManager.deployed();
+    });
+
+    // Note: Those tests are statistical, in some rare cases they may fail
+    describe('Statistical distribution of last digit', () => {
+      let occurencesOf = {};
+      let numberOfTries = 100;
+      let mean;
+
+      before(async () => {
+        jm = await JobManager.deployed();
+        for (let i = 0; i < numberOfTries; i++) {
+          const randomNumber = await jm.publicGetPseudoRandomNumber.call();
+          const lastDigit = randomNumber.mod(10).toNumber();
+          if (occurencesOf[lastDigit]) {
+            occurencesOf[lastDigit] += 1;
+          } else {
+            occurencesOf[lastDigit] = 1;
+          }
+          // Mine a block because so that block.number changes
+          await blockMiner.mine(1);
+        }
+      });
+
+      it('should have the correct mean', async () => {
+        mean = Object.keys(occurencesOf)
+          .map((lastDigit) => lastDigit * occurencesOf[lastDigit])
+          .reduce((a, b) => a + b) / numberOfTries;
+        // Theoretical mean is 4.5
+        const interval = 1;
+        assert(4.5 - interval < mean && mean < 4.5 + interval);
+      });
+
+      it('should have the correct variance', async () => {
+        const variance = Object.keys(occurencesOf)
+          .map((lastDigit) => occurencesOf[lastDigit] * (lastDigit - mean) ** 2)
+          .reduce((a, b) => a + b) / numberOfTries;
+        // Theoretical variance is 8.25
+        const interval = 2;
+        assert(8.25 - interval < variance && variance < 8.25 + interval);
+      });
+
+      it('should be a uniform distribution', async () => {
+        // Assert that every digit 0-9 appeared at least twice out of numberOfTries times (99% of chance)
+        for (lastDigit of Object.keys(occurencesOf)) {
+          assert(2 < occurencesOf[lastDigit]);
+        }
+      });
+    });
+  });
+
+  describe('selectProvider', () => {
+    before(async () => {
+      jm = await JobManager.deployed();
+    });
+  });
 });
