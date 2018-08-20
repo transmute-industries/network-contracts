f3dadcef995ff3016b6edc7297e4791a8389e9ec
gjgd <gjgd@protonmail.com>
2018-08-20 14:27:56 -0500

    Add tests for selectActiveProvider

--- test/unit/JobManager.spec.js
+++ test/unit/JobManager.spec.js
@@ -1,5 +1,5 @@
 const JobManager = artifacts.require('./TestJobManager.sol');
-const {assertFail, blockMiner} = require('../utils.js');
+const {assertFail, blockMiner, roundManagerHelper} = require('../utils.js');
 require('truffle-test-utils').init();
 
 contract('JobManager', (accounts) => {
@@ -188,8 +188,126 @@ contract('JobManager', (accounts) => {
   });
 
   describe('selectProvider', () => {
+    let jm;
+    let contractAddress;
+    const provider1 = accounts[1];
+    const provider2 = accounts[2];
+    const provider3 = accounts[3];
+    const nullAddress = 0;
+    const provider1Stake = 1;
+    const provider2Stake = 10;
+    const provider3Stake = 30;
+    const aFewTimes = 3;
+
+    async function approveBondProvider(pricePerStorageMineral, pricePerComputeMineral, blockRewardCut, feeShare, amountBonded, provider) {
+      await jm.approve(contractAddress, amountBonded, {from: provider});
+      await jm.bond(provider, amountBonded, {from: provider});
+      await jm.provider(pricePerStorageMineral, pricePerComputeMineral, blockRewardCut, feeShare, {from: provider});
+    }
+
     before(async () => {
-      jm = await JobManager.deployed();
+      jm = await JobManager.new();
+      contractAddress = jm.address;
+      for (let i = 0; i < 10; i++) {
+        await jm.mint(accounts[i], 10000, {from: accounts[0]});
+      }
+      const electionPeriodEndBlock = await roundManagerHelper.getElectionPeriodEndBlock(jm);
+      await blockMiner.mineUntilBlock(electionPeriodEndBlock);
+      await jm.initializeRound();
+      await jm.setNumberOfActiveProviders(4);
+      const pricePerStorageMineral = 22;
+      const provider1PricePerComputeMineral = 10;
+      const provider2PricePerComputeMineral = 12;
+      const provider3PricePerComputeMineral = 14;
+      const blockRewardCut = 1;
+      const feeShare = 25;
+      await approveBondProvider(pricePerStorageMineral, provider1PricePerComputeMineral,
+        blockRewardCut, feeShare, provider1Stake, provider1);
+      await approveBondProvider(pricePerStorageMineral, provider2PricePerComputeMineral,
+        blockRewardCut, feeShare, provider2Stake, provider2);
+      await approveBondProvider(pricePerStorageMineral, provider3PricePerComputeMineral,
+        blockRewardCut, feeShare, provider3Stake, provider3);
+    });
+
+    it('should return null address if there is no active Providers in the pool', async () => {
+      // New round is not initialized, so there are no active Providers yet
+      const activeProviderAddresses = await jm.getActiveProviderAddresses.call();
+      assert.equal(0, activeProviderAddresses.length);
+      const minPricePerMineral = 11;
+      const provider = await jm.publicSelectProvider.call(minPricePerMineral);
+      assert.equal(nullAddress, provider);
+    });
+
+    it('should select Providers proportionally to their relative stake', async () => {
+      // Initialize the active provider set
+      const newRoundBlock = await roundManagerHelper.getElectionPeriodEndBlock(jm);
+      await blockMiner.mineUntilBlock(newRoundBlock);
+      await jm.initializeRound();
+
+      const minPricePerMineral = 17;
+      const aLotOfTimes = 50;
+      const electedProvidersCount = {};
+      electedProvidersCount[provider1] = 0;
+      electedProvidersCount[provider2] = 0;
+      electedProvidersCount[provider3] = 0;
+      let i = 0;
+      while (i < aLotOfTimes) {
+        const provider = await jm.publicSelectProvider.call(minPricePerMineral);
+        electedProvidersCount[provider] += 1;
+        await blockMiner.mine(1);
+        i++;
+      }
+      const totalActiveStake = provider1Stake + provider2Stake + provider3Stake;
+      const provider1RelativeStake = provider1Stake / totalActiveStake;
+      const provider2RelativeStake = provider2Stake / totalActiveStake;
+      const provider3RelativeStake = provider3Stake / totalActiveStake;
+      const provider1ElectedRate = electedProvidersCount[provider1] / aLotOfTimes;
+      const provider2ElectedRate = electedProvidersCount[provider2] / aLotOfTimes;
+      const provider3ElectedRate = electedProvidersCount[provider3] / aLotOfTimes;
+      const threshold = 0.15;
+      assert.isAbove(threshold, Math.abs(provider1RelativeStake - provider1ElectedRate));
+      assert.isAbove(threshold, Math.abs(provider2RelativeStake - provider2ElectedRate));
+      assert.isAbove(threshold, Math.abs(provider3RelativeStake - provider3ElectedRate));
+    });
+
+    it('should not select Providers who have a pricePerMineral > minPricePerMineral', async () => {
+      const minPricePerMineral = 11;
+      // Even though provider1 has a tiny relative stake in the active Provider pool
+      // he will be selected every time because he is the only one who meets the
+      // price requirements
+      Array(aFewTimes).fill().forEach(async () => {
+        const provider = await jm.publicSelectProvider.call(minPricePerMineral);
+        assert.equal(provider1, provider);
+        await blockMiner.mine(1);
+      });
+    });
+
+    it('should select Providers who have a pricePerMineral == minPricePerMineral', async () => {
+      const minPricePerMineral = 12;
+      const electedProviders = [];
+      let i = 0;
+      // Try a few times to elect a new Provider
+      while (i < aFewTimes) {
+        const provider = await jm.publicSelectProvider.call(minPricePerMineral);
+        electedProviders.push(provider);
+        await blockMiner.mine(1);
+        i++;
+      }
+      // provider2 had more than 90% chance to be elected every time
+      // so we want to make sure it was selected at least once
+      // in the few tries we did
+      const provider2WasSelectedAtLeastOnce = electedProviders
+        .some((provider) => provider2 === provider);
+      assert.ok(provider2WasSelectedAtLeastOnce);
+    });
+
+    it('should return a null address if all Providers have pricePerMineral > minPricePerMineral', async () => {
+      const minPricePerMineral = 9;
+      Array(aFewTimes).fill().forEach(async () => {
+        const provider = await jm.publicSelectProvider.call(minPricePerMineral);
+        assert.equal(nullAddress, provider);
+        await blockMiner.mine(1);
+      });
     });
   });
 });
